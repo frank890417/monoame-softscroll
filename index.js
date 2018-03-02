bcbd52904f1926f797c9bb3744bdf27b3632bd08
cheyuwu <frank890417@gmail.com>
2018-03-02 17:44:31 +0800

    update workable version 1

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+let $ = require("jquery")
+let TweenMax = require("gsap").TweenMax
+let ScrollToPlugin = require("gsap/ScrollToPlugin")
+let enable = true
+export default {
+  enable: true,
+  init: function(){
+    var $window = $(window);
+    var scrollTime = 1;
+    var scrollDistance = 50;
+    $(function(){
+      $window.on("mousewheel DOMMouseScroll", function (event) {
+        if (enable) {
+          event.preventDefault();
+          var delta = event.originalEvent.wheelDelta / 40 || -event.originalEvent.detail / 0.5;
+          var scrollTop = $window.scrollTop();
+          var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+
+          TweenMax.to($window, scrollTime, {
+            scrollTo: { y: finalScroll, autoKill: true },
+            ease: Power2.easeOut,
+            overwrite: 10
+          });
+          // console.log(finalScroll);
+        }
+      });
+    })
+    
+  },
+  set(status){
+    enable=status
+  }
+}
 
